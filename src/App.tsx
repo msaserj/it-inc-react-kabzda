a32eaee235d7d3bf835cec52fa62127cbc4c77a8
msaserj <serg.ks@gmail.com>
2022-07-27 13:53:18 +0300

    08 React-kabzda callbacks for stars in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 import {OnOff} from "./components/OnOff/OnOff";
 import UncontrolledAccordion from "./components/UncontrolledAccordion/UncontrolledAccordion";
 import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
+import {Rating, RatingValueType} from "./components/Rating/Rating";
 
 function App() {
     console.log("App rendered")
+
+    let [rattingValue, setRatingValue] = useState<RatingValueType>(0)
+
     return (
         <div className="App">
-            {/*<PageTitle title={"This is APP component"}/>*/}
-            {/*Article 1*/}
-            {/*<UncontrolledRating value={3} />*/}
-            {/*<Accordion titleValue={"Menu"} collapsed={true} />*/}
-            {/*<Accordion titleValue={"Users"} collapsed={false} />*/}
-            {/*Article 2*/}
-            {/*<UncontrolledRating value={0} />*/}
-            {/*<UncontrolledRating value={1} />*/}
-            {/*<UncontrolledRating value={2} />*/}
-            {/*<UncontrolledRating value={3} />*/}
-            {/*<UncontrolledRating value={4} />*/}
-            {/*<UncontrolledRating value={5} />*/}
-            {/*<Test/>*/}
             <OnOff/>
             <OnOff/>
             <UncontrolledAccordion titleValue={"menu"}/>
             <UncontrolledAccordion titleValue={"users"}/>
             <UncontrolledRating  />
-            
+            <Rating value={rattingValue} onClick={setRatingValue}/>
         </div>
     );
 }
--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 
 
+export type RatingValueType = 0 | 1 | 2 | 3 | 4 | 5
+
+
 type RatingPropsType = {
     value: 0 | 1 | 2 | 3 | 4 | 5
+    onClick: (value: RatingValueType) => void
 }
 
 
@@ -10,24 +14,25 @@ export function Rating(props: RatingPropsType) {
     console.log("UncontrolledRating rendered")
     return (
         <div>
-            <Star selected={props.value > 0}/>
-            <Star selected={props.value > 1}/>
-            <Star selected={props.value > 2}/>
-            <Star selected={props.value > 3}/>
-            <Star selected={props.value > 4}/>
+            <Star selected={props.value > 0} onClick={props.onClick} value={1}/>
+            <Star selected={props.value > 1} onClick={props.onClick} value={2}/>
+            <Star selected={props.value > 2} onClick={props.onClick} value={3}/>
+            <Star selected={props.value > 3} onClick={props.onClick} value={4}/>
+            <Star selected={props.value > 4} onClick={props.onClick} value={5}/>
         </div>
     )
 }
 
 type StarPropsType = {
     selected: boolean
+    onClick: (value: RatingValueType) => void
+    value: RatingValueType
 }
 
-const Star = (props: StarPropsType) => props.selected
-    ? (<span> <b>star</b> </span>)
-    : (<span> star </span>)
+const Star = (props: StarPropsType) => {
+    return <span onClick={()=>{props.onClick(props.value)}}> {props.selected ? <b> star </b> : " star "} </span>
+}
 
-export const Test = () => <Star selected={true}/>
 
 export type TableTennisType = {
     tennisBall: (tennisBall: object) => void
