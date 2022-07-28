cc301143c16e1ab9175cb4326aa1494ad462c6f3
msaserj <serg.ks@gmail.com>
2022-07-29 01:27:40 +0300

    13 storybook for input.stories.tsx add checkbox select

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,7 +1,6 @@
 import React, {ChangeEvent, useRef, useState} from 'react';
 import {action} from "@storybook/addon-actions";
 
-
 export default {
     title: 'input stories',
    //component: UncontrolledRating
@@ -34,3 +33,32 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
 }
 
 export const  ControlledInputWithFixedValue = () => <input value="fixed value"/>
+
+export const  ControlledInput = () => {
+    const [parentValue, setParentValue] = useState('')
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+
+    return <input value={parentValue} onChange={onChange}/>
+}
+export const  ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(true)
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.checked)
+    }
+    return <input type="checkbox" checked={parentValue} onChange={onChange}/>
+}
+
+export const  ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>("2")
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+    return <select value={parentValue} onChange={onChange}>
+        <option>none</option>
+        <option value="1">Minsk</option>
+        <option value="2">Moskva</option>
+        <option value="3">Kostroma</option>
+    </select>
+}
