563eefebef9af7d3dbee5a01aa39cec90bd519c2
msaserj <serg.ks@gmail.com>
2022-07-29 00:48:59 +0300

    12 storybook for input.stories.tsx

---
+++ src/stories/input.stories.tsx
@@ -0,0 +1,34 @@
+import React, {ChangeEvent, useRef, useState} from 'react';
+import {action} from "@storybook/addon-actions";
+
+
+export default {
+    title: 'input stories',
+   //component: UncontrolledRating
+}
+
+const callback = action("Rating changed inside component")
+
+export const UncontrolledInput = () => <input />
+
+export const TrackValueUncontrolledInput = () => {
+    const[value, setValue] = useState('')
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => setValue(e.currentTarget.value)
+
+    return<>
+        <input onChange={onChange}/> {value}
+    </>
+}
+
+export const GetValueOfUncontrolledInputByButtonPress = () => {
+    const[value, setValue] = useState('')
+    const inputRef = useRef<HTMLInputElement>(null)
+    const save = () =>{
+        const el = inputRef.current as HTMLInputElement
+        setValue(el.value)
+    }
+    return<>
+
+        <input ref={inputRef} /> <button onClick={save}>SAVE</button> - actual value: {value}
+    </>
+}
